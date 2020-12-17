74f6c426e6cb8b4f9b02249bc134f87efe675de1
shmakov-il <ilia.eduardovich98@gmail.com>
2020-12-17 20:27:55 +0300

    create swiper reviews

--- js/main.js
+++ js/main.js
@@ -10,3 +10,38 @@ $(document).ready(function () {
     $(this).addClass('tabs-item--active');
   });
 });
+
+
+var autoSlider = document.querySelector('.swiper-container');
+  autoSlider.onmouseout = function(e) {
+    mySwiper.autoplay.start();
+  };
+    autoSlider.onmouseover = function(e) {
+    mySwiper.autoplay.stop();
+  };
+
+
+var mySwiper = new Swiper('.swiper-container', {
+  // Optional parameters
+  loop: true,
+
+  autoplay: {
+    delay: 7000,
+  },
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+})
